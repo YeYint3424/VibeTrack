8a61ed94b1955fc516ca9223caa35669bbd6db10
YeYint3424 <xino3424@gmail.com>
2023-06-08 14:23:09 +0630

    first commit

---
+++ src/main/java/com/vibetrack/vibetrack/VibetrackApplication.java
@@ -0,0 +1,13 @@
+package com.vibetrack.vibetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibetrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibetrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vibetrack/vibetrack/VibetrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibetrack.vibetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibetrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
